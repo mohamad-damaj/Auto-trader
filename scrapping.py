17afcb28dcbb949f09ccf6755be84bb7fed2e9ba
BrokenShadow53 <mohamadamaj123@gmail.com>
2025-06-26 02:21:03 -0400

    feat: added price and news fcns

--- scrapping.py
+++ scrapping.py
@@ -1,26 +1,18 @@
-import twint
 import yfinance as yf
 from dotenv import load_dotenv
 import os
 import requests
+from datetime import datetime
+import praw
 
 load_dotenv()
 
 finkey = os.getenv("FINNKEY")
 
-def get_tweets():
-    # Configure
-    c = twint.Config()
-    c.Search = "$TSLA"
-    c.Lang = "en"
-    c.Limit = 10
-    c.Pandas = True
-
-    # Run
-    twint.run.Search(c)
-    df = twint.storage.panda.Tweets_df
-
-    return df['tweet'].tolist()
+def get_reddit(ticker):
+    reddit = praw.reddit(
+        read_only = True
+    )
 
 
 
@@ -29,15 +21,18 @@ def get_news(ticker):
     response = requests.get(url)
     data = response.json()
 
-    return [item['headline'] for item in data]
+    return [(item['headline'], datetime.fromtimestamp(item['datetime']).strftime('%Y-%m-%d %H:%M:%S')) for item in data]
 
 def get_prices(TICKER):
     ticker = yf.Ticker(TICKER)
-    df = ticker.history(period="5d", interval="1h")
+    df = ticker.history(period="7d", interval="1h")
     return df
 
 
 if __name__=="__main__":
     news = get_news("AAPL")
+    prices = get_prices("AAPL")
+    tweets = get_tweets("AAPL")
 
-    print(news)
+
+    print(tweets)
